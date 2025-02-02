f9056ac46e159f4bc679d49cc8ba4acd0ed0b579
imprity <imprity041@gmail.com>
2025-02-02 09:52:39 +0900

    Added web crash handler.
    Now it displays crash log to web browser if it crashes.

--- web_build/scripts/loader.ts
+++ web_build/scripts/loader.ts
@@ -41,14 +41,19 @@ declare var MINESWEEPER_WASM_SIZE : number
         return new Uint8Array(data)
     }
 
+    // get document elements
     const loadingScreen = document.getElementById('loading-screen')
 
     const progressBar = document.getElementById('progress-bar')
     const progressFill = document.getElementById('progress-fill')
 
-    const errorTextBox = document.getElementById('error-text-box')
-    const errorText = document.getElementById('error-text')
+    const loadingErrorContainer = document.getElementById('loading-error-container')
+    const loadingErrorText = document.getElementById('loading-error-text')
 
+    const crashScreen = document.getElementById('crash-screen')
+    const crashErrortext = document.getElementById('crash-error-text')
+
+    // load wasm
     try {
         const data = await loadData(
             'minesweeper.wasm',
@@ -61,13 +66,76 @@ declare var MINESWEEPER_WASM_SIZE : number
 
         document.body.removeChild(loadingScreen)
 
+        // before instantiating go wasm
+        // override few methods and syscalls
+
+        function countNewLine (str : string) : number {
+            let counter = 0
+            let index = 0
+            while (true) {
+                const newIndex = str.indexOf("\n", index)
+                if (newIndex < 0) {
+                    break
+                }else {
+                    counter++
+                }
+                index = newIndex + 1
+            }
+
+            return counter
+        }
+
+        const originalWriteSync = globalThis.fs.writeSync
+	    const decoder = new TextDecoder("utf-8");
+
+        let logText = ""
+        const logTextMax = 500
+
+        let newLineCounter = 0
+
+        globalThis.fs.writeSync = function(fd, buf) {
+            let bufText = decoder.decode(buf)
+            const newLineCount = countNewLine(bufText)
+            newLineCounter += newLineCount
+
+            logText += bufText
+
+            if (newLineCounter > logTextMax) {
+                const diff = newLineCounter - logTextMax
+                let index = 0
+                for (let i=0; i<diff; i++) {
+                    const newIndex = logText.indexOf("\n", index)
+                    if (newIndex < 0) {
+                        break
+                    }
+                    index = newIndex + 1
+                }
+                if (index < 0) {
+                    index = 0
+                }
+
+                logText = logText.substring(index)
+
+                newLineCounter = logTextMax
+            }
+
+            return originalWriteSync(fd, buf)
+        }
+
         const go = new Go()
+        go.exit = (code) => {
+            if (code != 0) {
+                crashScreen.style.display = 'flex' // show crashScreen
+                crashErrortext.innerText = logText
+            }
+        }
+
         WebAssembly.instantiate(new Uint8Array(data), go.importObject).then(instnace=>{
             go.run(instnace.instance)
         })
     } catch(err) {
-        errorTextBox.style.display = 'block' // show errorTextBox
-        errorText.innerText = err.toString()
+        loadingErrorContainer.style.display = 'block' // show error container
+        loadingErrorText.innerText = err.toString()
         console.error(err)
     }
 })()
